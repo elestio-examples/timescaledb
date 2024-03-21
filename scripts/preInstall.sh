4d116d287f437f05ce42b5cf038ee3ddbf40229f
David <30422759+davalimi@users.noreply.github.com>
2024-03-21 12:40:01 +0200

    Update preInstall.sh

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -24,10 +24,10 @@ cat <<EOT > ./servers.json
 }
 EOT
 
-docker-compose up -d;
-sleep 20s;
-docker-compose down
-openssl req -new -x509 -days 3650 -nodes -text -out ./data/server.crt -keyout ./data/server.key -subj "/CN=${CNAME}"
-# sed --debug -i 's/#//g' ./docker-compose.yml
-sleep 5s;
-sed --debug  -i '/^\s*# command/s/# //' ./docker-compose.yml
+# docker-compose up -d;
+# sleep 20s;
+# docker-compose down
+# openssl req -new -x509 -days 3650 -nodes -text -out ./data/server.crt -keyout ./data/server.key -subj "/CN=${CNAME}"
+# # sed --debug -i 's/#//g' ./docker-compose.yml
+# sleep 5s;
+# sed --debug  -i '/^\s*# command/s/# //' ./docker-compose.yml
