05f2724dbc56f62ca780769286e03e59caeeec90
David <30422759+davalimi@users.noreply.github.com>
2024-03-21 12:33:14 +0200

    Update preInstall.sh

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -29,4 +29,5 @@ sleep 20s;
 docker-compose down
 openssl req -new -x509 -days 3650 -nodes -text -out ./data/server.crt -keyout ./data/server.key -subj "/CN=${CNAME}"
 # sed --debug -i 's/#//g' ./docker-compose.yml
-sed -i '/^\s*# command/s/# //' ./docker-compose.yml
+sleep 5s;
+sed --debug  -i '/^\s*# command/s/# //' ./docker-compose.yml
