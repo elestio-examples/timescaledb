e7b8e0e6c99cd6e9652d027419c21011c356967d
David <30422759+davalimi@users.noreply.github.com>
2024-03-21 16:47:11 +0200

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -2,8 +2,6 @@ set -o allexport; source .env; set +o allexport;
 
 docker-compose down
 openssl req -new -x509 -days 3650 -nodes -text -out ./data/server.crt -keyout ./data/server.key -subj "/CN=${CNAME}"
-# sed --debug -i 's/#//g' ./docker-compose.yml
 sleep 5s;
-sed --debug  -i '/^\s*# command/s/# //' ./docker-compose.yml
+sed -i 's/#command:/command:/' docker-compose.yml
 docker-compose up -d;
-
